649a7e08a3da759f70f06671c04d7d77d9b3cf0b
Torbjörn Lönnemark <tobbez@ryara.net>
2009-05-31 15:43:25 +0200

    Clean up code in problem 12

--- 12/12.c
+++ 12/12.c
@@ -6,21 +6,18 @@ int main()
 {
   int counter = 0;
   long long triangle = 0;
-  int i;
+  int i, factors;
   while(1)
   {
     ++counter;
     triangle += counter;
     
-    int factors = 0;
-    for(i = 1; i <= sqrt(triangle) + 1; ++i)
+    factors = 0;
+    for(i = 1; i <= sqrt(triangle); ++i)
       if(triangle % i == 0) factors += 2;
     if(factors > 500) break;
-    
-    //if(counter % 500 == 0)
-    /*    printf("%i:%li:%i\n", counter, triangle, factors); */
-    
   }
+
   printf("\nthe number is: %li\n",triangle);
   return 0;
 }
